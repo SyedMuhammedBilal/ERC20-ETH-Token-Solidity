a707d6a6bd9a4497dc89ec86ee98bf69700416f5
"SyedMuhammedBilal" <"intergo878@gmail.com">
2022-01-20 18:02:46 +0500

    New Functions

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
+import "./ERC20Interface.sol";
 import "./SafeMath.sol";
 
-contract ERC20 {
+contract ERC20 is ERC20Interface {
     
     using SafeMath for uint256;
 
@@ -12,13 +13,11 @@ contract ERC20 {
 
     uint256 internal totalSupply_;
 
-    event Transfer(address indexed _sender, address indexed _to, uint256 _value);
-
     function totalSupply() external view returns (uint256) {
         return totalSupply_;
     }
 
-    function transfer(address _to, uint256 _value) external returns (bool) {
+    function transfer(address _to, uint256 _value) public override returns (bool) {
         require(_value <= balances[msg.sender]);
         require(_to != address(0));
 
@@ -28,4 +27,25 @@ contract ERC20 {
         return true;
     }
     
+    function approve(address _spender, uint256 _value) public override returns (bool) {
+        // prevents Front-Running Attack
+        require(_value == 0 || allowed[msg.sender][_spender] == 0);
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool) {
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+        require(_to != address(0));
+
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
 }
